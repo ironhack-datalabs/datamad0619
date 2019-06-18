93e58650dd1d0df8466eff4695eea20df0a460f2
delia <deliaclarramirez@gmail.com>
2019-06-18 19:29:16 +0200

    BONUS

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -6,6 +6,7 @@ SELECT authors.au_id as AUTHOR_ID ,authors.au_lname AS LAST_NAME , authors.au_fn
 	INNER JOIN titles ON titles.title_id=titleauthor.title_id
 	INNER JOIN publishers ON titles.pub_id=publishers.pub_id
 	ORDER BY authors.au_id;
+	
 #CHALLENGE 2
 SELECT AUTHOR_ID, LAST_NAME, FIRST_NAME,PUBLISHER,COUNT(*) AS TITLE_COUNT
 	FROM (
@@ -37,3 +38,12 @@ SELECT authors.au_id as AUTHOR_ID , authors.au_lname AS LAST_NAME , authors.au_f
 	LEFT JOIN sales ON sales.title_id=titles.title_id
 	GROUP BY AUTHOR_ID , LAST_NAME,FIRST_NAME ORDER BY SUM(sales.qty) DESC;
 
+
+#BONUS
+SELECT authors.au_id as AUTHOR_ID , authors.au_lname AS LAST_NAME , authors.au_fname AS FIRST_NAME, 
+IFNULL((SUM(IFNULL(sales.qty,0))*titles.royalty*(titleauthor.royaltyper/100)+titles.advance),0) AS PROFIT
+	FROM authors
+	LEFT JOIN titleauthor ON authors.au_id=titleauthor.au_id
+	LEFT JOIN titles ON titles.title_id=titleauthor.title_id
+	LEFT JOIN sales ON sales.title_id=titles.title_id
+	GROUP BY AUTHOR_ID , LAST_NAME,FIRST_NAME ORDER BY PROFIT DESC;
