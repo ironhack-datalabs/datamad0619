99f1df46d9d8b56203af44c281544e763f9bd29b
fserrey <fserrey@gmail.com>
2019-06-18 21:26:53 +0200

    limit inserted in C3

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -39,5 +39,11 @@ INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, publishers.pub_id
+ORDER BY TOTAL DESC
+LIMIT 3;
 
-GROUP BY authors.au_id, publishers.pub_id;
+
+#Challenge 4
+
+
