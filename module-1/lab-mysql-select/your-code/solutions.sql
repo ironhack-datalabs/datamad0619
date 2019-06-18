6a71164b625067220d72cd9812f6dc3fa47042da
Pacoanes <fanesvag@gmail.com>
2019-06-18 19:38:19 +0200

    challenge 4 hecho

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -2,7 +2,7 @@
 		authors.au_lname AS "LAST NAME", 
 		authors.au_fname AS "FIRST NAME", 
 		publishers.pub_name AS "PUBLISHERS",
-		SUM(sales.qty) AS "TOTAL"
+		IFNULL(SUM(sales.qty), 0) AS "TOTAL"
 		 FROM authors 
 		   INNER JOIN titleauthor ON titleauthor.au_id = authors.au_id
 		   INNER JOIN titles ON titles.title_id = titleauthor.title_id
@@ -10,6 +10,6 @@
            LEFT JOIN sales ON titles.title_id = sales.title_id
 		   GROUP BY authors.au_id, titles.pub_id
            ORDER BY SUM(sales.qty) DESC
-           LIMIT 3;
+           LIMIT 23;
            
     
