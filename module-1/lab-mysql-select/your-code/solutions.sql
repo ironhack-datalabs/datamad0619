feb5bfe9378528cb8ced1b08446085187b4f0feb
fserrey <fserrey@gmail.com>
2019-06-18 18:47:11 +0200

    Challenge 1 done

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,24 @@
+USE publications;
+
+ #Challenge 1
+
+SELECT authors.au_id AS `AUTHOR_ID`, 
+		authors.au_lname AS `LAST_NAME`,
+    	authors.au_fname AS `FIRST_NAME`, 
+		titles.title AS `TITLE`,
+        publishers.pub_name AS `PUBLISHER`
+        
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+
+#Challenge 2
+
+SELECT authors.au_id AS `AUTHOR_ID`, 
+		authors.au_lname AS `LAST_NAME`,
+    	authors.au_fname AS `FIRST_NAME`, 
+		titles.title AS `TITLE`,
+        publishers.pub_name AS `PUBLISHER`
+
+    
