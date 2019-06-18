d641d560fec88c184b2da5624a09a1f1000b6f03
Marta Gracia <mgraciaverdura@gmail.com>
2019-06-18 20:22:12 +0200

    Challenge 2 acabado, aunque me sale un error que no entiendo y me gustaría revisar con vosotros :-)

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -1,11 +1,12 @@
-USE ´publications´;
+USE publications;
 
-#CHALLENGE 1
+-- CHALLENGE 1
 
 SELECT authors.au_id AS `AUTHOR ID`,
 authors.au_lname AS `LAST NAME`,
 authors.au_fname AS `FIRST NAME`,
-titles.title
+titles.title AS `TITLE`,
+publishers.pub_name AS `PUBLISHER`
 FROM authors
 INNER JOIN
 titleauthor
@@ -18,5 +19,20 @@ titleauthor.title_id=titles.title_id
 INNER JOIN
 publishers
 ON
-titles.pub_id=publishers.pub_id
-;
+titles.pub_id=publishers.pub_id;
+
+-- CHALLENGE 2
+
+SELECT authors.au_id AS `AUTHOR_ID`,
+authors.au_lname AS `LAST_NAME`,
+authors.au_fname AS `FIRST_NAME`,
+titles.title AS `TITLE`,
+publishers.pub_name AS `PUBLISHER`,
+COUNT(titles.title_id) AS `TITLE COUNT`
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id, publishers.pub_id
+
+-- CHALLENGE 3
