06967cac49dd5bb30f08e7ffef23bff311bc8cb5
noeliacarrion <ncs.933@gmail.com>
2019-06-18 18:55:12 +0200

    hasta challenge 2

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,16 @@
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', titles.title as 'TITLE',  publishers.pub_name as 'PUBLISHER'  FROM authors 
+	INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+	INNER JOIN titles ON titleauthor.title_id = titles.title_id
+    INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+
+ SELECT authors.au_id as 'AUTHOR ID', 
+	authors.au_lname as 'LAST NAME', 
+	authors.au_fname as 'FIRST NAME',  
+	publishers.pub_name as 'PUBLISHER' ,
+	COUNT(titles.title) as 'TITLE COUNT'
+	FROM authors 
+		INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+		INNER JOIN titles ON titleauthor.title_id = titles.title_id
+		INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+
+GROUP BY authors.au_id, publishers.pub_id;
