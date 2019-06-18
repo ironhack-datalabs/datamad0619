94f0d660d24baeab8ea11615bb075e931fd6bad0
fserrey <fserrey@gmail.com>
2019-06-18 21:17:06 +0200

    3 challenge done

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -19,18 +19,25 @@ SELECT authors.au_id AS `AUTHOR_ID`,
 		authors.au_lname AS `LAST_NAME`,
     	authors.au_fname AS `FIRST_NAME`, 
 		titles.title AS `TITLE`, 
-        publishers.pub_id AS `PUBLISHER`,
-        COUNT(titles.title) AS `TITLE COUNT`
+        publishers.pub_name AS `PUBLISHER`,
+        COUNT(titles.title_id) AS `TITLE COUNT`
 FROM authors
 INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
-GROUP BY authors.au_id, publishers.pub_id
+GROUP BY authors.au_id, publishers.pub_id;
 
-#INNER JOIN titles ON COUNT(titles.title), GROUP BY titleauthor.au_id, publishers.pub_name     
+#Challenge 3
 
-#SELECT attribute1, COUNT(attribute2)
-#FROM someTable
-#GROUP BY attribute1
-#HAVING COUNT(attribute2) > 2;
-    
+SELECT 	authors.au_id AS `AUTHOR_ID`, 
+		authors.au_lname AS `LAST_NAME`,
+    	authors.au_fname AS `FIRST_NAME`, 
+		titles.title AS `TITLE`,
+        SUM(sales.qty) AS `TOTAL`
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+INNER JOIN sales ON titles.title_id = sales.title_id
+
+GROUP BY authors.au_id, publishers.pub_id;
