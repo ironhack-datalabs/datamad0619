feae30eab71be883b5690bc09c829722bd9b2575
Fonsiman <aromanbonachera@gmail.com>
2019-06-18 23:00:42 +0200

    Solución de errores y 2 bonus (inventados) añadidos

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -36,19 +36,47 @@ SUM(qty) AS `TOTAL`
 SELECT a.au_id AS `AUTHOR ID`, au_lname AS `LAST NAME`, au_fname AS `FIRST NAME`,
 IFNULL(SUM(qty), 0) AS `TOTAL`
 	FROM authors AS a 
-	INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
-	INNER JOIN titles AS t ON ta.title_id = t.title_id
-    INNER JOIN sales AS s ON t.title_id = s.title_id
+	LEFT JOIN titleauthor AS ta ON a.au_id = ta.au_id
+	LEFT JOIN titles AS t ON ta.title_id = t.title_id
+    LEFT JOIN sales AS s ON t.title_id = s.title_id
     GROUP BY a.au_id
     ORDER BY TOTAL DESC;
     
 # BONUS CHALLENGE
 
 SELECT a.au_id AS `AUTHOR ID`, au_lname AS `LAST NAME`, au_fname AS `FIRST NAME`,
-IFNULL(ROUND((SUM(qty)*price*royalty/100+advance)*royaltyper/100, 2),0) AS `PROFIT`
+IFNULL(ROUND(SUM(qty*price*royalty/100*royaltyper/100) + SUM(advance*royaltyper/100)/COUNT(t.title_id), 2), 0) AS `PROFIT`
 	FROM authors AS a 
 	INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
 	INNER JOIN titles AS t ON ta.title_id = t.title_id
     INNER JOIN sales AS s ON t.title_id = s.title_id
     GROUP BY a.au_id
+    ORDER BY PROFIT DESC
+    LIMIT 3;
+    
+# BONUS CHALLENGE 2
+# ¿CUÁNTO GANAN LOS EDITORES CON CADA AUTOR?
+
+SELECT a.au_id AS `AUTHOR ID`, au_lname AS `LAST NAME`, au_fname AS `FIRST NAME`,
+IFNULL(ROUND((SUM(qty)*price*(100-royalty)/100-advance), 2),0) AS `PROFIT`
+	FROM authors AS a 
+	LEFT JOIN titleauthor AS ta ON a.au_id = ta.au_id
+	INNER JOIN titles AS t ON ta.title_id = t.title_id
+    INNER JOIN sales AS s ON t.title_id = s.title_id
+    GROUP BY a.au_id, t.title_id
     ORDER BY PROFIT DESC;
+    
+# BONUS CHALLENGE 3
+# ¿CUÁNTOS LIBROS TIENEN QUE VENDERSE PARA EMPEZAR A SER RENTABLES?
+
+SELECT  `TITLE ID`, `TITLE`, NECESARIOS, VENDIDOS, NECESARIOS - VENDIDOS AS FALTAN
+	FROM (
+	SELECT t.title_id AS `TITLE ID`, title AS `TITLE`,
+	ROUND(advance/(price*(100-royalty))*100, 0) AS NECESARIOS,
+	SUM(qty) AS VENDIDOS
+		FROM titles AS t
+		INNER JOIN sales AS s ON t.title_id = s.title_id
+		GROUP BY s.title_id
+	) summary
+	ORDER BY FALTAN ASC;
+
