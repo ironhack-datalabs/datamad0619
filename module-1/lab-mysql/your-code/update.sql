fd2260d4c568711356b222847108a29e2340810e
Alfonso-AML <aalvarezmiranda@gmail.com>
2019-06-18 15:37:04 +0200

    Ejercicios bonus completados

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,5 @@
+UPDATE SALESPERSONS SET STORE="Miami" WHERE STORE="Mimia";
+
+UPDATE CUSTOMERS SET Email="ppicasso@gmail.com" WHERE Full_name="Pablo Picasso";
+UPDATE CUSTOMERS SET Email="lincoln@us.gov" WHERE Full_name="Abraham Lincoln";
+UPDATE CUSTOMERS SET Email="hello@napoleon.me" WHERE Full_name="Napoléon Bonaparte";
