b52fab5ca679ac42ba957c5811cb89b219a51ed7
Pacoanes <fanesvag@gmail.com>
2019-06-18 19:19:53 +0200

    challange 3

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,11 +1,15 @@
  SELECT authors.au_id AS "AUTHOR", 
 		authors.au_lname AS "LAST NAME", 
 		authors.au_fname AS "FIRST NAME", 
-		publishers.pub_id AS "PUBLISHERS",
-		COUNT(titles.title) AS "TITLE COUNT"
-		 FROM authors
+		publishers.pub_name AS "PUBLISHERS",
+		SUM(sales.qty) AS "TOTAL"
+		 FROM authors 
 		   INNER JOIN titleauthor ON titleauthor.au_id = authors.au_id
 		   INNER JOIN titles ON titles.title_id = titleauthor.title_id
 		   INNER JOIN publishers ON publishers.pub_id = titles.pub_id
-		   GROUP BY authors.au_id, publishers.pub_id;
+           LEFT JOIN sales ON titles.title_id = sales.title_id
+		   GROUP BY authors.au_id, titles.pub_id
+           ORDER BY SUM(sales.qty) DESC
+           LIMIT 3;
+           
     
