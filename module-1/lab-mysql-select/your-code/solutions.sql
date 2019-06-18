d34db1f6ccd1e4b1a602e731fd00e7de82a8e689
fserrey <fserrey@gmail.com>
2019-06-18 21:31:30 +0200

    C4 - all 23 authors displayed

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -46,4 +46,16 @@ LIMIT 3;
 
 #Challenge 4
 
+SELECT 	authors.au_id AS `AUTHOR_ID`, 
+		authors.au_lname AS `LAST_NAME`,
+    	authors.au_fname AS `FIRST_NAME`, 
+		titles.title AS `TITLE`,
+        SUM(sales.qty) AS `TOTAL`
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, publishers.pub_id
+ORDER BY TOTAL ASC;
 
