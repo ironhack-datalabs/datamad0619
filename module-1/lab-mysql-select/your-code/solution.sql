c2e6d9203c685ea1950feb31d4ceff64fdf395ab
Fonsiman <aromanbonachera@gmail.com>
2019-06-19 09:28:32 +0200

    Correción errores

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -44,16 +44,21 @@ IFNULL(SUM(qty), 0) AS `TOTAL`
     
 # BONUS CHALLENGE
 
+SELECT  `AUTHOR ID`, `LAST NAME`, `FIRST NAME`, ROUND(SUM(ROYALTIES) + SUM(ADVANCE), 2) AS PROFIT
+	FROM (
 SELECT a.au_id AS `AUTHOR ID`, au_lname AS `LAST NAME`, au_fname AS `FIRST NAME`,
-IFNULL(ROUND(SUM(qty*price*royalty/100*royaltyper/100) + SUM(advance*royaltyper/100)/COUNT(t.title_id), 2), 0) AS `PROFIT`
+IFNULL(ROUND(SUM(qty*price*royalty/100*royaltyper/100), 2), 0) AS ROYALTIES,
+advance*royaltyper/100 AS ADVANCE
 	FROM authors AS a 
 	INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
 	INNER JOIN titles AS t ON ta.title_id = t.title_id
     INNER JOIN sales AS s ON t.title_id = s.title_id
-    GROUP BY a.au_id
-    ORDER BY PROFIT DESC
-    LIMIT 3;
-    
+    GROUP BY a.au_id, t.title_id
+) summary 
+GROUP BY `AUTHOR ID`
+ORDER BY PROFIT DESC
+LIMIT 3;
+
 # BONUS CHALLENGE 2
 # ¿CUÁNTO GANAN LOS EDITORES CON CADA AUTOR?
 
@@ -79,4 +84,3 @@ SELECT  `TITLE ID`, `TITLE`, NECESARIOS, VENDIDOS, NECESARIOS - VENDIDOS AS FALT
 		GROUP BY s.title_id
 	) summary
 	ORDER BY FALTAN ASC;
-
