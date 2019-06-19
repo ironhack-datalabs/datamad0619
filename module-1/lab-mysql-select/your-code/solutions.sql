225e1924f26a524906c6d74666aae4e1f7afd3cf
Maximiliano Sanchez <max.sanchez.dp@gmail.com>
2019-06-19 10:13:04 +0200

    improved all solutions

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,27 +1,29 @@
-#TEMPORARY TABLE
-CREATE TEMPORARY TABLE au_title_pub
+#CHALLENGE 1
 SELECT ta.au_id as AUTHOR_ID, a.au_lname AS LAST_NAME, 
-a.au_fname AS FIRST_NAME, t.title AS TITLE, ta.title_id as TITLE_ID, p.pub_name AS PUBLISHER
+a.au_fname AS FIRST_NAME, t.title AS TITLE, p.pub_name AS PUBLISHER
 FROM titleauthor as ta
 LEFT JOIN authors as a ON ta.au_id=a.au_id
 LEFT JOIN titles as t ON ta.title_id=t.title_id
 LEFT JOIN publishers as p ON t.pub_id=p.pub_id
 ORDER BY a.au_id;
 
-#CHALLENGE 1
-SELECT AUTHOR_ID, LAST_NAME, FIRST_NAME, TITLE, PUBLISHER FROM au_title_pub;
-
 #CHALLENGE 2
-SELECT AUTHOR_ID, LAST_NAME, FIRST_NAME, PUBLISHER, COUNT(TITLE) AS TITLE_COUNT
-FROM au_title_pub
-GROUP BY AUTHOR_ID, LAST_NAME, FIRST_NAME, PUBLISHER
+SELECT ta.au_id as AUTHOR_ID, a.au_lname AS LAST_NAME, 
+a.au_fname AS FIRST_NAME, p.pub_name AS PUBLISHER, COUNT(t.title) as TITLE_COUNT
+FROM titleauthor as ta
+LEFT JOIN authors as a ON ta.au_id=a.au_id
+LEFT JOIN titles as t ON ta.title_id=t.title_id
+LEFT JOIN publishers as p ON t.pub_id=p.pub_id
+GROUP BY AUTHOR_ID, PUBLISHER
 ORDER BY TITLE_COUNT DESC;
 
 #CHALLENGE 3
-SELECT atp.AUTHOR_ID, atp.LAST_NAME, atp.FIRST_NAME, SUM(s.qty) AS TOTAL
-FROM au_title_pub as atp
-LEFT JOIN sales as s ON atp.TITLE_ID=s.title_id
-GROUP BY atp.AUTHOR_ID, atp.LAST_NAME, atp.FIRST_NAME
+SELECT ta.au_id as AUTHOR_ID, a.au_lname AS LAST_NAME, 
+a.au_fname AS FIRST_NAME, SUM(s.qty) AS TOTAL
+FROM titleauthor as ta
+LEFT JOIN authors as a ON ta.au_id=a.au_id
+LEFT JOIN sales as s ON ta.title_id=s.title_id
+GROUP BY AUTHOR_ID
 ORDER BY TOTAL DESC
 LIMIT 3;
 
@@ -30,26 +32,21 @@ SELECT a.au_id as AUTHOR_ID, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME, 
 FROM authors as a
 LEFT JOIN titleauthor as ta ON a.au_id=ta.au_id
 LEFT JOIN sales as s ON ta.title_id = s.title_id
-GROUP BY AUTHOR_ID, LAST_NAME, FIRST_NAME
+GROUP BY AUTHOR_ID
 ORDER BY TOTAL DESC;
 
 #BONUS: MOST PROFITING AUTHORS
 
-#TABLA TEMPORAL
-CREATE TEMPORARY TABLE tabla_datos
-SELECT ta.title_id as TITLE_ID, ta.au_id as AUT_ID, ta.royaltyper/100 as ROY_AUT, t.royalty/100 AS ROY_TITLE, 
-t.price AS PRICE_TITLE, t.advance AS ADV_TITLE, SUM(s.qty) AS QTY_TITLE,
-((t.royalty*t.price*SUM(s.qty)/100)+t.advance)*(ta.royaltyper/100) as PROFIT 
-FROM titleauthor as ta 
-LEFT JOIN titles as t ON ta.title_id=t.title_id
-LEFT JOIN sales as s ON ta.title_id=s.title_id
-GROUP BY TITLE_ID, AUT_ID
-ORDER BY ta.title_id;
-
-#TABLA DEFINITIVA
-SELECT a.au_id as AUTHOR_ID, a.au_lname as "NAME", a.au_fname as LAST_NAME, SUM(td.PROFIT) AS PROFIT
-FROM tabla_datos as td
-LEFT JOIN authors as a ON td.AUT_ID=a.au_id
-GROUP BY AUTHOR_ID
+SELECT AUT_ID, LAST_NAME, FIRST_NAME, SUM(PROFIT) AS PROFIT
+FROM(
+	SELECT ta.title_id as TITLE_ID, ta.au_id as AUT_ID, a.au_lname as LAST_NAME, a.au_fname AS FIRST_NAME,
+	((t.royalty*t.price*SUM(s.qty)/100)+t.advance)*(ta.royaltyper/100) as PROFIT 
+	FROM titleauthor as ta 
+	LEFT JOIN titles as t ON ta.title_id=t.title_id
+	LEFT JOIN sales as s ON ta.title_id=s.title_id
+	LEFT JOIN authors as a on ta.au_id=a.au_id
+	GROUP BY TITLE_ID, AUT_ID
+	)summary
+GROUP BY AUT_ID
 ORDER BY PROFIT DESC
 LIMIT 3;
