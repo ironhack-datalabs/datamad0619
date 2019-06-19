ea31a92aaffb962dcdc92aba1a242d843376dda1
aiborra11 <iborra.11@gmail.com>
2019-06-19 08:02:28 +0200

    challenge 2 arreglado

--- module-1/lab-mysql-select/solution.sql
+++ module-1/lab-mysql-select/solution.sql
@@ -17,17 +17,18 @@ SELECT authors.au_id AS 'AUTHOR ID',
 		ON titles.pub_name = publishers.pub_id
 		GROUP BY authors.au_id, publishers.pub_name;
 
-#challenge2   
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT authors.au_id AS 'AUTHOR ID', 
-	authors.au_lname AS 'LAST NAME',
-    authors.au_fname AS 'FIRST NAME',
-    sales.sum(qty) AS 'TOTAL SALES'
-    
-    FROM authors
-    INNER JOIN titleauthor
-		ON 	authors.au_id = titleauthor.au_id
-	INNER JOIN sales
-		ON titleauthor.title_id = sales.title_id
-        GROUP BY authors.au_id
-		;
+#Challenge2
+SELECT authors.au_id AS 'Author ID', 
+	authors.au_fname AS 'AUTHOR NAME', 
+	authors.au_lname AS 'AUTHOR LASTNAME', 
+	publishers.pub_name AS 'PUBLISHER',
+	COUNT(titles.title_id) AS 'TITLE COUNT'
+
+	FROM authors
+	INNER JOIN titleauthor
+		ON authors.au_id = titleauthor.au_id
+	INNER JOIN titles
+		ON  titleauthor.title_id= titles.title_id
+	INNER JOIN publishers
+		ON titles.pub_id = publishers.pub_id
+    	GROUP BY authors.au_id, publishers.pub_name;
