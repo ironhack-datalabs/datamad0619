9231818f1d14b3b4b5b7b48a1f3f4b0ac672d5a1
aiborra11 <iborra.11@gmail.com>
2019-06-19 09:34:53 +0200

    challenge 3

--- module-1/lab-mysql-select/solution.sql
+++ module-1/lab-mysql-select/solution.sql
@@ -1,5 +1,5 @@
 
-#challenge1
+#Challenge1
 SELECT authors.au_id AS 'AUTHOR ID', 
 	authors.au_fname AS 'AUTHOR NAME', 
 	authors.au_lname AS 'AUTHOR LASTNAME', 
@@ -19,16 +19,31 @@ SELECT authors.au_id AS 'AUTHOR ID',
 
 #Challenge2
 SELECT authors.au_id AS 'Author ID', 
-	authors.au_fname AS 'AUTHOR NAME', 
-	authors.au_lname AS 'AUTHOR LASTNAME', 
-	publishers.pub_name AS 'PUBLISHER',
-	COUNT(titles.title_id) AS 'TITLE COUNT'
+authors.au_fname AS 'AUTHOR NAME', 
+authors.au_lname AS 'AUTHOR LASTNAME', 
+publishers.pub_name AS 'PUBLISHER',
+COUNT(titles.title_id) AS 'TITLE COUNT'
 
-	FROM authors
-	INNER JOIN titleauthor
-		ON authors.au_id = titleauthor.au_id
-	INNER JOIN titles
-		ON  titleauthor.title_id= titles.title_id
-	INNER JOIN publishers
-		ON titles.pub_id = publishers.pub_id
-    	GROUP BY authors.au_id, publishers.pub_name;
+FROM authors
+INNER JOIN titleauthor
+	ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+	ON  titleauthor.title_id= titles.title_id
+INNER JOIN publishers
+	ON titles.pub_id = publishers.pub_id
+    GROUP BY authors.au_id, publishers.pub_name;
+      
+#Challenge3
+SELECT authors.au_id AS 'AUTHOR ID', 
+	authors.au_lname AS 'LAST NAME',
+    authors.au_fname AS 'FIRST NAME',
+    sum(sales.qty) AS 'TOTAL SALES'
+    
+    FROM authors
+    INNER JOIN titleauthor
+		ON 	authors.au_id = titleauthor.au_id
+	INNER JOIN sales
+		ON titleauthor.title_id = sales.title_id
+        GROUP BY authors.au_id
+        ORDER BY sum(sales.qty) DESC
+        LIMIT 3;
