5b8468474dfff616dbbb7e7d07878d9bdf184a56
noeliacarrion <ncs.933@gmail.com>
2019-06-19 09:35:51 +0200

    cambios challenge 4

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -40,17 +40,13 @@ LIMIT 3;
 
 '''CHallenge 4'''
 
-
  SELECT authors.au_id as 'AUTHOR ID', 
 	authors.au_lname as 'LAST NAME', 
 	authors.au_fname as 'FIRST NAME',  
-	IFNULL(publishers.pub_name, 0) as 'PUBLISHER' ,
-	COUNT(titles.title) as 'TITLE COUNT',
     IFNULL(SUM(sales.qty), 0) as 'TOTAL'
 	FROM authors 
 		LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
 		LEFT JOIN titles ON titleauthor.title_id = titles.title_id
         LEFT JOIN sales  ON titles.title_id = sales.title_id 
-		LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
-GROUP BY authors.au_id, publishers.pub_id
+GROUP BY authors.au_id
 ORDER BY TOTAL DESC
