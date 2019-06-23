e8202743955b7816e83de4f94a13c3582a86ee64
Fonsiman <aromanbonachera@gmail.com>
2019-06-23 17:54:00 +0200

    Añadiendo bonus

--- module-1/pandas-project/your-code/myfunctions.py
+++ module-1/pandas-project/your-code/myfunctions.py
@@ -60,89 +60,121 @@ def create_sizes(spe):
 def cleanspecies(spe):
     spe = spe.lower()
     if "unknown" in spe:
-        return "Unknown"
+        return "UNKNOWN"
     elif "tiger" in spe and "sand" in spe:
-        return "Sand tiger shark"
+        return "Sand tiger"
     elif "tiger" in spe:
-        return "Tiger shark"
+        return "Tiger"
     elif "whitetip" in spe:
-        return "Oceanic whitetip shark"
+        return "Oceanic whitetip"
     elif "white" in spe:
-        return "White shark"
+        return "White"
     elif "hammerhead" in spe:
-        return "Great hammerhead shark"
+        return "Great hammerhead"
     elif "wobbegong" in spe:
-        return "Wobbegong shark"
+        return "Wobbegong"
     elif "blue" in spe:
-        return "Blue shark"
+        return "Blue"
     elif "blacktip" in spe and "reef" in spe:
-        return "Blacktip reef shark"
+        return "Blacktip reef"
     elif "blacktip" in spe or "blackfin" in spe or ("black" in spe and ("tip" in spe or "tipped" in spe))\
     or "limbatus" in spe:
-        return "Blacktip shark"
+        return "Blacktip"
     elif "leopard" in spe:
-        return "Leopard shark"
+        return "Leopard"
     elif "caribbean" in spe:
-        return "Caribbean reef shark"
+        return "Caribbean reef"
     elif "bronze whale" in spe or "copper" in spe or "narrowtooth" in spe:
-        return "Bronze whaler shark"
+        return "Bronze whaler"
     elif "raggedtooth" in spe:
-        return "Raggedtooth shark"
+        return "Raggedtooth"
     elif "mako" in spe or "bonita" in spe or "bonito" in spe:
-        return "Shortfin mako shark"
+        return "Shortfin mako"
     elif "zambezi" in spe or "zambesi" in spe or "zambi" in spe or "bull" in spe or "leucas" in spe:
-        return "Zambezi shark"
+        return "Zambezi"
     elif "nurse" in spe:
-        return "Nurse shark"
+        return "Nurse"
     elif "spinner" in spe:
-        return "Spinner shark"
+        return "Spinner"
     elif "dusky" in spe:
-        return "Dusky shark"
+        return "Dusky"
     elif "sandbar" in spe or "brown-colored" in spe:
-        return "Sandbar shark"
+        return "Sandbar"
     elif "lemon" in spe:
-        return "Lemon shark"
+        return "Lemon"
     elif "horn" in spe:
-        return "Horn shark"
+        return "Horn"
     elif "banjo" in spe:
-        return "Banjo shark"
+        return "Banjo"
     elif "galapago" in spe:
-        return "Galapagos shark"
+        return "Galapagos"
     elif "silky" in spe:
-        return "Silky shark"
+        return "Silky"
     elif "gray" in spe or "grey" in spe:
-        return "Gray reef shark"
+        return "Gray reef"
     elif "broadnose" in spe or "sevengill" in spe or "seven-gill" in spe or ("seven" in spe and "gill" in spe):
-        return "Broadnose sevengill shark"
+        return "Broadnose sevengill"
     elif "bluntnose" in spe or "sixgill" in spe:
-        return "Bluntnose sixgill shark"
+        return "Bluntnose sixgill"
     elif "spotted" in spe or "wobbegong" in spe or "carpet" in spe:
-        return "Spotted Wobbegong shark"
+        return "Spotted Wobbegong"
     elif "basking" in spe:
-        return "Basking shark"
+        return "Basking"
     elif "reef" in spe:
-        return "Reef shark"
+        return "Reef"
     elif "sand" in spe:
-        return "Sand shark"
+        return "Sand"
     elif "soup" in spe or "school" in spe or "snapper" in spe or "tope" in spe:
-        return "School shark"
+        return "School"
     elif "whale" in spe:
-        return "Whale shark"
+        return "Whale"
     elif "goblin" in spe:
-        return "Goblin shark"
+        return "Goblin"
     elif "salmon" in spe:
-        return "Salmon shark"
+        return "Salmon"
     elif "spiny" in spe or "dogfish" in spe or "spurgog" in spe or "mud" in spe or "piked" in spe or "dog" in spe:
         return "Spiny dogfish"
     elif "angel" in spe:
         return "Angelshark"
     elif "porbeagle" in spe:
-        return "Porbeagle shark"
+        return "Porbeagle"
     elif "thresher" in spe or "fox" in spe:
-        return "Fox shark"
+        return "Fox"
     elif "cookie" in spe or "cutter" in spe:
-        return "Cookiecutter shark"
+        return "Cookiecutter"
     elif "small" in spe or "little" in spe or "juvenile" in spe or "young" in spe:
-        return "Small shark"
+        return "Small"
     else:
-        return "Unknown"
+        return "UNKNOWN"
+
+def cleaninjury(inj):
+    inj = inj.lower()
+    if "unknown" in inj:
+        return "UNKNOWN"
+    elif ("no" in inj and ("injur" in inj or "inur" in inj or "ijnur" in inj)) or "molested" in inj or (("holed" in inj or "hole" in inj) and "boat" in inj):
+        return "No injuries"
+    elif ("no" in inj and "body" in inj) or "miss" in inj or "kill" in inj or "dead" in inj or "consume" in inj \
+    or "died" in inj or "scaveng" in inj or "perish" in inj:
+        return "Dead"
+    elif "fatal" in inj or "amputat" in inj or "sever" in inj or "serious" in inj or "massive" in inj \
+    or "remove" in inj or "lost" in inj or "avuls" in inj or "bared" in inj or "mutilate" in inj:
+        return "Seriously injured"
+    elif "minor" in inj or "lacerat" in inj or "cut" in inj or "abrasion" in inj or "nipp" in inj \
+    or "bruise" in inj or "blow" in inj or "graze" in inj or "scratch" in inj or "abraded" in inj or "small" in inj\
+    or "hit" in inj or "pinch" in inj:
+        return "Lacerations"
+    elif "bit" in inj or "puncture" in inj or "injur" in inj or "gash" in inj or "wound" in inj \
+    or "toothmark" in inj or "grasp" in inj or "grab" in inj or "torn" in inj or "injur" in inj \
+    or "inur" in inj or "fractur" in inj or "broke" in inj:
+        return "Injured"
+    else:
+        return "UNKNOWN"
+
+def cleantype(typ):
+    typ = typ.lower()
+    if "boating" in typ:
+        return "Boat"
+    elif "unknown" in typ:
+        return "UNKNOWN"
+    else:
+        return typ.capitalize()
