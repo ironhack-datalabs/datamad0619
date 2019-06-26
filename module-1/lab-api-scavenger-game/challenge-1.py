c9042067572fe196c6bbd990d5d00dea2f2933e7
Emma Machado <emma.machado@gmail.com>
2019-06-26 23:34:37 +0200

    Challenge 1 completada

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,1 +1,28 @@
-# enter your code below
+
+import requests
+import json
+
+with open('/Users/emachado/datamad0619/module-1/lab-api-scavenger-game/token.txt', 'r') as file:
+    token = file.read()
+
+headers = {
+  "Authorization": "token {}".format(token)
+}
+
+BASE_URL = "https://api.github.com"
+
+owner = "ironhack-datalabs"
+repo = "madrid-oct-2018"
+
+url = "{}/repos/{}/{}/forks".format(BASE_URL, owner, repo)
+res = requests.get(url, headers=headers)
+data = res.json()
+
+def languages(fork):
+    fork_language = []
+    for e in data:
+        if e["language"] not in fork_language:
+            fork_language.append(e["language"])
+    return fork_language
+
+print(languages(data))
