540fa938db80a072ea8bf2b5c6986d71450abf58
Marco Rueda <marcorueda92z@gmail.com>
2019-06-26 19:00:27 +0200

    Challenge 1

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,1 +1,25 @@
 # enter your code below
+
+import requests
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+
+if not "GITHUB_TOKEN" in os.environ:
+    raise ValueError("You should pass a GITHUB TOKEN, see: https://github.com/settings/tokens")
+
+BASE_URL = "https://api.github.com"
+
+owner = "ironhack-datalabs"
+repo = "mad-oct-2018"
+
+res = requests.get("{}/repos/{}/{}/languages".format(BASE_URL,owner, repo))
+res = res.json()
+
+
+
+
+
