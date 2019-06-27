ef570f35748120c7441e30c381ad8987a071e1df
delia <deliaclarramirez@gmail.com>
2019-06-27 10:15:07 +0200

    Mejoras

--- module-1/lab-api-scavenger-game/challenge3.py
+++ module-1/lab-api-scavenger-game/challenge3.py
@@ -15,13 +15,9 @@ BASE_URL = "https://api.github.com"
 repo="ironhack-datalabs/scavenger"
 res=requests.get("{}/repos/{}/contents".format(BASE_URL,repo),auth=("deliaclar",GITHUB_TOKEN))
 a=res.json()
-print(len(a))
-print(len(a[0]))
+
 dir=[e.get("path") for e in a if e.get("type")=="dir"]
-#for e in a:
- #   if e.get("type")=="dir":
-  #      dir.append(e.get("path"))
-print(dir)
+
 path2=[]
 letras=[]
 orden=[]
@@ -33,20 +29,16 @@ for e in dir:
             if "scavengerhunt" in e3.get("name"):
                 orden.append(int(e3.get("name")[1:5]))
                 path2.append(e3.get("download_url"))
-
+d=dict()
 gcontext = ssl.SSLContext()
+i=0
 for e5 in path2:
     leer=p.urlopen(e5,context=gcontext)
-    letras.append(leer.read())
-print(letras)
-print(orden)
-d=dict()
-for i in range(len(orden)):
-    d[str(orden[i])]=str(letras[i],"utf-8")
-letrasOrdenadas=[]
-for i in range(len(orden)):
-    letrasOrdenadas.append(d[str(i+1)])
+    d[str(orden[i])]=str(leer.read(),"utf-8")
+    i+=1
 
+
+letrasOrdenadas=[d[str(i+1)] for i in range(len(orden))]
 print(" ".join(letrasOrdenadas).replace("\n"," "))
 
 
